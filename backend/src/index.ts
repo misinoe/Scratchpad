79da7049912a9f9613404868f802012a4b951a4f
misinoe <7781818+misinoe@users.noreply.github.com>
2019-09-23 20:29:00 +0900

    backend

---
+++ backend/src/index.ts
@@ -0,0 +1,103 @@
+#!/usr/share/node
+const HTTP_PORT = parseInt(process.env['HTTP_PORT']) || 8888;
+if (isNaN(HTTP_PORT)) {
+  throw new Error('invalid HTTP_PORT from ENV');
+  process.exit(1);
+}
+
+import http from 'http';
+import express from 'express';
+import socketIo, {Namespace, Socket} from 'socket.io';
+
+const app = express();
+const httpServer = new http.Server(app);
+const io = socketIo(httpServer);
+
+httpServer.listen(HTTP_PORT);
+
+app.use(express.static('./static'));
+
+class Room {
+  private dataIndex = 0;
+  private sharedData = {};
+
+  private maxTime = 3;
+  private time = this.maxTime;
+
+  private socketMap = new Map<string, Socket>();
+  private socketIo: Namespace;
+
+  constructor(socketIo: Namespace) {
+    this.socketIo = socketIo;
+    socketIo.on('connection', this.clientConnectionHandler.bind(this));
+
+    this.notifyTimer();
+  }
+
+  private clear = () => {
+    const {socketIo, sharedData} = this;
+
+    const deleteNameList: string[] = [];
+    for (let name in sharedData) {
+      delete sharedData[name];
+      deleteNameList.push(name);
+    }
+
+    socketIo.emit('delete', deleteNameList);
+  }
+
+  private notifyTimer = () => {
+    const time = this.time --;
+    if (time <= 0) {
+      this.clear();
+      this.time = this.maxTime;
+    } else {
+      this.socketIo.emit('changed', {name: 't', data: this.time})
+    }
+
+    setTimeout(this.notifyTimer, 3000);
+  }
+
+  private clientConnectionHandler = (clientSocket: Socket) => {
+    const {socketMap, socketIo} = this;
+    const {id} = clientSocket;
+    if (socketMap.has(id)) {
+      clientSocket.disconnect();
+      return;
+    }
+    socketMap.set(id, clientSocket);
+    clientSocket.on('disconnect', (reason) => {
+      socketMap.delete(id);
+      socketIo.emit('changed', {name: 'u', data: socketMap.size});
+    });
+
+    clientSocket.on('a', (data) => {
+      if (typeof data !== 'string') {
+        clientSocket.disconnect();
+        return;
+      }
+      const name = 'a' + (++ this.dataIndex);
+      this.sharedData[name] = data;
+
+      socketIo.emit('changed', {name, data});
+    });
+
+    const {sharedData} = this;
+    for (let name in sharedData) {
+      clientSocket.emit('changed', {name, data: sharedData[name]});
+    }
+
+    socketIo.emit('changed', {name: 'u', data: socketMap.size});
+  }
+}
+
+const room1 = new Room(io.of('/1'));
+const room2 = new Room(io.of('/2'));
+const room3 = new Room(io.of('/3'));
+
+io.on('connection', socket => {
+  
+  socket.on('a', data => {
+    console.log(data);
+  })
+});
